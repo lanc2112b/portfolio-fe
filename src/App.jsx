e4661cb0517d4a64a56c6d03e73cfdeb320816e3
Tony Lancaster <lanc1968@gmail.com>
2023-04-22 16:03:09 +0100

    Add structure components. Add PortfolioSection to check appearance.

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import MainNav from "./components/navs/MainNav";
+import Header from "./components/structure/Header";
+import PortfolioSection from './components/portfolio/PortfolioSection';
+import Footer from './components/structure/Footer';
+import MainSection from './components/structure/MainSection';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [sideBar, setSideBar] = useState(true);
 
   return (
     <>
-      <p className="read-the-docs">
-        Routing here.
-      </p>
+      <div className="wrapper w-full min-h-screen p-0 m-0 flex flex-row">
+        <MainNav sideBar={sideBar} />
+
+        <div className="center-wrapper w-full bg-gradient-to-tr from-stone-50 via-stone-100 to-zinc-300">
+          <Header setSideBar={setSideBar} sideBar={sideBar} />
+
+            <MainSection component={<PortfolioSection />} />
+
+          <Footer />
+        </div>
+      </div>
     </>
   )
 }
