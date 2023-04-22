828811a7cd8d0969a66d7bbd242e335102686f9d
Tony Lancaster <lanc1968@gmail.com>
2023-04-22 16:35:03 +0100

    Add basic ContactForm component

---
+++ src/components/contact/ContactForm.jsx
@@ -0,0 +1,21 @@
+const ContactForm = () => {
+
+    return (
+        <>
+            <section className="container gap-4 columns-1 
+                                 
+                                py-10 mx-auto min-h-screen
+                                px-3 shad-hint">
+                
+                <form>
+                    <textarea name="" id="" cols="30" rows="10">
+                        
+                    </textarea>
+                </form>
+                
+            </section>
+        </>
+    );
+}
+
+export default ContactForm;
