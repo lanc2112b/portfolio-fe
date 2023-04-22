d0a7ebe6b616d446d4530dadc2d6d26bc4133057
Tony Lancaster <lanc1968@gmail.com>
2023-04-22 16:04:17 +0100

    Add basic footer component

---
+++ src/components/structure/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    
+    return (
+        <>
+            <footer className="w-full h-40 bg-gradient-to-tr from-slate-800 via-slate-700 to-slate-900">
+
+            </footer>
+        </>
+    )
+}
+
+export default Footer;
