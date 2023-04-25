9b068cae890d86824a71b55b2ed9c633cb2a3e0a
Tony Lancaster <lanc1968@gmail.com>
2023-04-25 17:08:21 +0100

    Refactor validation. Refactor async recaptcha token function. Refactor submitHandler Refactor onChangeHandler Add toast messages for success / warning & error messages.

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -1,34 +1,16 @@
-import { useEffect, useState, useCallback } from "react";
-import {useGoogleReCaptcha} from 'react-google-recaptcha-v3';
-import {postContactsForm} from "../../api/apiConsumer";
+import { useEffect, useState } from "react";
+import { useGoogleReCaptcha } from 'react-google-recaptcha-v3';
+import { toast } from "react-hot-toast";
+import Bread from "../uiparts/Bread";
+import { postContactsForm } from "../../api/apiConsumer";
 
 const ContactForm = () => {
+
+    const { executeRecaptcha } = useGoogleReCaptcha();
 
     useEffect(() => {
         document.title = 'Get In Touch';
     });
-
-    const [gToken, setGToken] = useState('');
-    
-    const { executeRecaptcha } = useGoogleReCaptcha();
-
-    const handleReCaptchaVerify = useCallback(async () => {
-        if (!executeRecaptcha) {
-            //console.log('Execute recaptcha not yet available');
-            return;
-        }
-
-        const token = await executeRecaptcha('contact_form');
-
-        setGToken(token);
-
-        //console.log(token);
-
-    }, [executeRecaptcha]);
-
-    useEffect(() => {
-        handleReCaptchaVerify();
-    }, [handleReCaptchaVerify]);
 
     const initial = {
         name: '',
@@ -40,159 +22,200 @@ const ContactForm = () => {
         token: '',
     };
 
-    const [formObj, setFormObj] = useState({...initial});
-    const [formErrors, setFormErrors] = useState({...initial, errors: null});
+    const [formObj, setFormObj] = useState({ ...initial });
+    const [formErrors, setFormErrors] = useState({ ...initial, errors: null });
 
     const submitHandler = (event) => {
 
         event.preventDefault();
-        
-        const tmpObj = { ...formErrors };
 
-        tmpObj.errors = null;
+        if (!executeRecaptcha) {
+            console.log('Execute recaptcha not yet available');
+            return;
+        }
 
-        if (Object.values(formErrors).join('')) {
+        setFormErrors((currentObj) => { return { ...currentObj, errors: null } });
 
-            tmpObj.errors = "Check form errors, highlighted in red."
-            setFormErrors(tmpObj);
+        if (!formObj.name ||
+            !formObj.email ||
+            !formObj.subject ||
+            !formObj.query) {
+            
+            const msg = {
+                type: 'warning',
+                title: 'Empty Form',
+                msg: 'Fill in the form and try again',
+            }
+
+            toast.custom(t => (<Bread msgObj={msg} t={t} />));
+            return;
+        }
+
+        if (Object.values(formErrors).join('') !== '') {
+
+            setFormErrors((currentObj) => { return { ...currentObj, errors: 'Check form errors, highlighted in red' } });
             return;
 
         }
 
-        setFormErrors(tmpObj);
-
-        formObj.token = gToken;
-
-        postContactsForm(formObj)
+        executeRecaptcha('contact_form')
             .then((result) => {
+                const postObj = { ...formObj, token: result };
+                return postContactsForm(postObj);
+            })
+            .then((result) => {
+                if (result.status === 201) {
+                    //console.log('posted successfully');
+                    const msg = {
+                        type: 'success',
+                        title: 'Submitted',
+                        msg: 'Message submitted, I\'ll get back to you ASAP',
+                    }
+
+                    toast.custom(t => (<Bread msgObj={msg} t={t} />));
+                    resetHandler();
+                }
                 //console.log(result);
-                handleReCaptchaVerify();
-                resetHandler();
             })
             .catch((error) => {
-                console.log(error);
-            });
+                if (error.response.status === 400) {
+                    const msg = {
+                        type: 'warning',
+                        title: 'Bad request',
+                        msg: 'Correct any form errors and try again',
+                    }
 
+                    toast.custom(t => (<Bread msgObj={msg} t={t} />));
+                    
+                } else {
+                    const msg = {
+                        type: 'error',
+                        title: 'Error',
+                        msg: 'Something went wrong, your message could not be submitted',
+                    }
+
+                    toast.custom(t => (<Bread msgObj={msg} t={t} />));
+                }
+            });
     }
 
     const changeHandler = (event) => {
 
-        const currentTrack = event.target.name;
-        const currentVal = event.target.value;
-        const newObj = { ...formObj };
+        const { name, value } = event.target;
 
-        newObj[currentTrack] = currentVal;
-        setFormObj(newObj);
-        validateFormItem(currentTrack);
+        validateFormItem(name, value);
+        //console.log(`${name} and ${value} from change handler`);
+
+        setFormObj((currentObj) => { return { ...currentObj, [name]: value } });
+
     }
 
-    function validateFormItem(name) {
-
-        const tmpObj = { ...formErrors };
-
-        tmpObj.errors = null;
+    function validateFormItem(name, value) {
 
         if (name === 'name') {
 
-            tmpObj.name = null;
+            setFormErrors((currErrs) => { return { ...currErrs, name: '' } });
 
-            if (!formObj.name) {
-                tmpObj.name = 'Name is required';
+            if (!value) {
+                setFormErrors((currErrs) => { return { ...currErrs, name: 'Full name is required' } });
 
             } else {
 
-                if (formObj.name.length > 0 && formObj.name.length < 4) {
-                    tmpObj.name = 'Name must have more than 4 characters';
+                if (value.length > 0 && value.length <= 4) {
+                    setFormErrors((currErrs) => { return { ...currErrs, name: 'Full name needs more than 4 characters' } });
                 }
 
-                if (formObj.name.length > 255) {
-                    tmpObj.name = 'Name must have less than 255 characters';
+                if (value.length > 255) {
+                    setFormErrors((currErrs) => { return { ...currErrs, name: 'Maximum of 255 characters for full name' } });
                 }
 
-                if (/[^-a-zA-Z\s]/.test(formObj.name)) {
-                    tmpObj.name = 'Name can only contain letters';
-                } 
+                if (/[^-a-zA-Z\s]/.test(value)) {
+                    setFormErrors((currErrs) => { return { ...currErrs, name: 'Full name can only contain letters & spaces' } });
+                }
             }
         }
 
         if (name === 'email') {
 
-            tmpObj.email = null;
+            setFormErrors((currErrs) => { return { ...currErrs, email: '' } });
 
-            if (!formObj.email) {
-                tmpObj.email = 'Email is required';
+            if (!value) {
+
+                setFormErrors((currErrs) => { return { ...currErrs, email: 'Email is required' } });
 
             } else {
 
-                if (formObj.email.length > 0 && formObj.email.length < 3) {
-                    tmpObj.email = 'Email must have more than 3 characters';
+                if (value.length > 0 && value.length < 3) {
+                    setFormErrors((currErrs) => { return { ...currErrs, email: 'Email must have more than 3 characters' } });
                 }
 
-                if (formObj.email.length > 255) {
-                    tmpObj.email = 'Email must have less than 255 characters';
+                if (value.length > 255) {
+                    setFormErrors((currErrs) => { return { ...currErrs, email: 'Email must have less than 255 characters' } });
                 }
+
+                // TODO: add valid email... 
             }
         }
 
         if (name === 'subject') {
 
-            tmpObj.subject = null;
+            setFormErrors((currErrs) => { return { ...currErrs, subject: '' } });
 
-            if (!formObj.subject) {
-                tmpObj.subject = 'A subject is required';
+            if (!value) { 
+
+                setFormErrors((currErrs) => { return { ...currErrs, subject: 'A subject is required' } });
 
             } else {
 
-                if (formObj.subject.length > 0 && formObj.subject.length < 10) {
-                    tmpObj.subject = 'Subject must have more than 10 characters';
+                if (value.length > 0 && value.length < 10) {
+                    setFormErrors((currErrs) => { return { ...currErrs, subject: 'Subject must have more than 10 characters' } });
                 }
 
-                if (formObj.subject.length > 255) {
-                    tmpObj.subject = 'Subject must have less than 255 characters';
+                if (value.length > 255) {
+                    setFormErrors((currErrs) => { return { ...currErrs, subject: 'Subject must have less than 255 characters' } });
                 }
             }
         }
 
         if (name === 'source') {
 
-            tmpObj.source = null;
+            setFormErrors((currErrs) => { return { ...currErrs, source: '' } });
 
-            if (formObj.source) {
+            if (value) {
 
-                if (formObj.source.length > 0 && formObj.source.length < 10) {
-                    tmpObj.source = 'Source must have more than 10 characters';
+                if (value.length > 0 && value.length < 10) {
+                    setFormErrors((currErrs) => { return { ...currErrs, source: 'Source must have more than 10 characters' } });
                 }
 
-                if (formObj.source.length > 255) {
-                    tmpObj.source = 'URL must have less than 255 characters';
+                if (value.length > 255) {
+                    setFormErrors((currErrs) => { return { ...currErrs, source: 'Source must have less than 255 characters' } });
                 }
             }
         }
 
         if (name === 'query') {
 
-            tmpObj.query = null;
+            setFormErrors((currErrs) => { return { ...currErrs, query: '' } });
 
-            if (!formObj.query) {
-                tmpObj.query = 'A query is required';
+            if (!value) {
+                setFormErrors((currErrs) => { return { ...currErrs, query: 'A query is required' } });
 
             } else {
 
-                if (formObj.query.length > 0 && formObj.query.length < 10) {
-                    tmpObj.query = 'Query must have more than 10 characters';
+                if (value.length > 0 && value.length < 10) {
+                    setFormErrors((currErrs) => { return { ...currErrs, query: 'Query must have more than 10 characters' } });
                 }
 
-                if (formObj.query.length > 255) {
-                    tmpObj.query = 'Query must have less than 255 characters';
+                if (value.length > 255) {
+                    setFormErrors((currErrs) => { return { ...currErrs, query: 'Query must have less than 255 characters' } });
                 }
             }
         }
 
-        setFormErrors(tmpObj);
     }
 
     const resetHandler = () => {
-        
+
         setFormObj({ ...initial });
         setFormErrors({ ...initial });
     }
@@ -208,20 +231,20 @@ const ContactForm = () => {
                 <form onSubmit={submitHandler}>
                     <div className="grid gap-4 grid-cols-2">
                         <div className="col-span-2 sm:col-span-1 flex flex-col">
-                            <label className="font-medium ms-1" htmlFor="name">Name (full)*: <span className="ms-2 font-normal text-sm text-red-600">{formErrors.name}</span></label>
+                            <label className="font-medium ms-1" htmlFor="name">*Name (fullname): <span className="ms-2 font-normal text-sm text-red-600">{formErrors.name}</span></label>
                             <input type="text" value={formObj.name} onChange={changeHandler} name="name" id="name" placeholder="Full Name" className="my-1 px-3
                                                                                           border-0 border-b-2 border-gray-200
                                                                                           focus:ring-0 focus:border-stone-400" />
                         </div>
 
                         <div className="col-span-2 sm:col-span-1 flex flex-col">
-                            <label className="font-medium ms-1" htmlFor="email">Email*: <span className="ms-2 font-normal text-sm text-red-600">{formErrors.email}</span></label>
+                            <label className="font-medium ms-1" htmlFor="email">*Email: <span className="ms-2 font-normal text-sm text-red-600">{formErrors.email}</span></label>
                             <input type="email" value={formObj.email} onChange={changeHandler} name="email" id="email" placeholder="Email Address" className="my-1 px-3
                                                                                           border-0 border-b-2 border-gray-200
                                                                                           focus:ring-0 focus:border-stone-400" />
                         </div>
                         <div className="col-span-2 sm:col-span-1 flex flex-col">
-                            <label className="font-medium ms-1" htmlFor="subject">Subject*:
+                            <label className="font-medium ms-1" htmlFor="subject">*Subject:
                                 <span className="ms-2 font-normal text-sm text-red-600">
                                     {formErrors.subject}
                                 </span>
@@ -231,13 +254,13 @@ const ContactForm = () => {
                                                                                           focus:ring-0 focus:border-stone-400" />
                         </div>
                         <div className="col-span-2 sm:col-span-1 flex flex-col">
-                            <label className="font-medium ms-1" htmlFor="source">Source*: <span className="ms-2 font-normal text-sm text-red-600">{formErrors.source}</span></label>
+                            <label className="font-medium ms-1" htmlFor="source">Source: <span className="ms-2 font-normal text-sm text-red-600">{formErrors.source}</span></label>
                             <input type="text" value={formObj.source} onChange={changeHandler} name="source" id="source" placeholder="How Did You Find Me?" className="my-1 px-3
                                                                                           border-0 border-b-2 border-gray-200
                                                                                           focus:ring-0 focus:border-stone-400" />
                         </div>
                         <div className="col-span-2 flex flex-col">
-                            <label className="font-medium ms-1" htmlFor="query">Query*: <span className="ms-2 font-normal text-sm text-red-600">{formErrors.query}</span></label>
+                            <label className="font-medium ms-1" htmlFor="query">*Query: <span className="ms-2 font-normal text-sm text-red-600">{formErrors.query}</span></label>
                             <textarea name="query" value={formObj.query} onChange={changeHandler} id="query" cols="30" rows="10" placeholder="What Can I Help You With?" className=" my-1 px-3
                                                                                           border-0 border-b-2 border-gray-200
                                                                                           focus:ring-0 focus:border-stone-400">
@@ -246,8 +269,10 @@ const ContactForm = () => {
                             <span className="ms-0.5 text-sm">Character count: {formObj.query.length} (max 1500)</span>
                         </div>
                         <input type="hidden" value={formObj.clrchck} onChange={changeHandler} name="clrchck" id="clrchck" className="" />
+                        <div className="col-span-2 flex flex-row justify-end pe-2 me-3"><span className="ms-2 font-semibold text-red-600">{formErrors.errors}</span></div>
                         <div className="col-span-2 flex flex-row justify-end pe-2 me-3">
-                            <button type="submit" className="me-4 py-1 px-3 font-semibold rounded-full" onClick={resetHandler}>
+                            
+                            <button type="button" className="me-4 py-1 px-3 font-semibold rounded-full" onClick={resetHandler}>
                                 Reset
                             </button>
                             <button type="submit" className="bg-stone-400 py-1 px-3 w-full sm:w-auto hover:animate-pulse shadow-sm hover:shadow-md rounded-full font-semibold text-stone-50 hover:bg-stone-400">
@@ -255,7 +280,7 @@ const ContactForm = () => {
                                 Send
                             </button>
                         </div>
-                        
+
                     </div>
                 </form>
 
