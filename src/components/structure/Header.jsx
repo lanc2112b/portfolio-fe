d1778d279f8c5e5b2607780762bd171f7e0c0b47
Tony Lancaster <lanc1968@gmail.com>
2023-04-28 10:27:31 +0100

    Remove light/dark toggle button temporarily.

--- src/components/structure/Header.jsx
+++ src/components/structure/Header.jsx
@@ -12,12 +12,12 @@ const Header = ({ sideBar, setSideBar }) => {
             <header className="h-7 px-3 w-full sticky top-0 border-1 z-50 bg-stone-50 shadow-sm flex flex-row justify-between items-center">
                 <button type="button" onClick={navHandler}><i className="fa-solid fa-bars"></i></button>
                 
-                <div>
+                {/* <div>
                     <i className="fa-regular fa-lightbulb"></i>
                     <i className="fa-solid fa-lightbulb"></i>
                     <i className="fa-solid fa-toggle-on"></i>
                     <i className="fa-solid fa-toggle-off"></i>
-                </div>
+                </div> */}
             </header>
             <Toaster
                 position="top-right"
