7fada256f069055c8d52cf42b4137176763450e0
Tony Lancaster <lanc1968@gmail.com>
2023-04-28 17:39:57 +0100

    Change footer text

--- src/components/structure/Footer.jsx
+++ src/components/structure/Footer.jsx
@@ -2,8 +2,8 @@ const Footer = () => {
     
     return (
         <>
-            <footer className="w-full h-10 text-xl z-50 mt-auto bg-stone-100 border-t border-stone-200"> {/** bg-gradient-to-tr from-slate-800 via-slate-700 to-slate-900 */}
-                <div className="w-full h-full flex flex-row text-slate-700 justify-center items-center">&copy; T Lancaster 2023</div>
+            <footer className="w-full h-10 z-50 mt-auto bg-stone-100 border-t border-stone-200"> {/** bg-gradient-to-tr from-slate-800 via-slate-700 to-slate-900 */}
+                <div className="w-full h-full flex flex-row text-slate-700 justify-center items-center">T Lancaster <span className="text-lg">&nbsp;{' '}&copy;2023</span></div>
                 
             </footer>
         </>
