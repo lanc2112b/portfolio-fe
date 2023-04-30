fa15c680e2eb89327c24bbb11a28e413b724f028
Tony Lancaster <lanc1968@gmail.com>
2023-04-30 09:37:35 +0100

    Add some overrides for code blocks.

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,10 +13,29 @@ export default {
       screens: {
         '3xl': '2100px',
       },
-},
+
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            '--tw-prose-links': theme('colors.teal[600]'),
+            code: {
+              backgroundColor: theme("colors.stone.200"),
+              color: theme('colors.slate[700]'),
+              fontWeight: "400",
+              "border-radius": "0.25rem",
+              "padding": "0.2rem 0.4rem",
+              "margin-inline": "0.1rem",
+              "font-family": '"Courier New", Courier, "Lucida Sans Typewriter", "Lucida Typewriter", monospace',
+            },
+          },
+        },
+      }),
+    },
+    
   },
   plugins: [
     require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
   ],
 }
 
