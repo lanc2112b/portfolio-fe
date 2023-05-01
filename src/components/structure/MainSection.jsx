230bda6fb3a4d76bd1f17f27b5b0780ebeb20c35
Tony Lancaster <lanc1968@gmail.com>
2023-05-01 17:45:59 +0100

    undo last commit

--- src/components/structure/MainSection.jsx
+++ src/components/structure/MainSection.jsx
@@ -5,9 +5,9 @@ const MainSection = ({ component }) => {
             <main className="w-full h-full m-0 p-0">
                 {component}
             </main>
-            <div className="w-4/5 my-3 mx-auto">
+            {/* <div className="w-4/5 my-3 mx-auto">
                 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3213202004633527" crossorigin="anonymous"></script>
-            </div>
+            </div> */}
         </>
     );
 
