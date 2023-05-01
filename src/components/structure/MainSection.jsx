2eac871c42bd753721f7f1526cc5b0b34d324349
Tony Lancaster <lanc1968@gmail.com>
2023-05-01 17:44:08 +0100

    Add google adsense code.

--- src/components/structure/MainSection.jsx
+++ src/components/structure/MainSection.jsx
@@ -1,10 +1,15 @@
-const MainSection = ({component}) => {
+const MainSection = ({ component }) => {
 
-    return (<>
-        <main className="w-full h-full m-0 p-0">
-            {component}
-        </main>
-    </>)
+    return (
+        <>
+            <main className="w-full h-full m-0 p-0">
+                {component}
+            </main>
+            <div className="w-4/5 my-3 mx-auto">
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3213202004633527" crossorigin="anonymous"></script>
+            </div>
+        </>
+    );
 
 }
 
