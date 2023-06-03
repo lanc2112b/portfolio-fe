f5c3782e52eba3b200ce083099bf4e678496237d
Tony Lancaster <lanc1968@gmail.com>
2023-06-03 14:59:59 +0100

    feat: add dark mode toggle button

--- src/components/structure/Header.jsx
+++ src/components/structure/Header.jsx
@@ -1,23 +1,45 @@
+import { useContext } from 'react';
+import { ThemeContext } from '../../contexts/Theme';
 import { Toaster } from 'react-hot-toast';
 
 const Header = ({ sideBar, setSideBar }) => {
+
+    const { theme, setTheme } = useContext(ThemeContext);
 
     const navHandler = () => {
 
         setSideBar(!sideBar);
     }
 
+    const toggleTheme = () => {
+
+        setTheme(prev => { return !prev });
+
+    }
+
     return (
         <>
             <header className="h-7 px-3 w-full sticky top-0 border-1 z-50 bg-stone-50 shadow-sm flex flex-row justify-between items-center">
-                <button type="button" onClick={navHandler}><i className="fa-solid fa-bars"></i></button>
-                
-                {/* <div>
-                    <i className="fa-regular fa-lightbulb"></i>
-                    <i className="fa-solid fa-lightbulb"></i>
-                    <i className="fa-solid fa-toggle-on"></i>
-                    <i className="fa-solid fa-toggle-off"></i>
-                </div> */}
+                <div>
+                    <button type="button" onClick={navHandler}><i className="fa-solid fa-bars me-3"></i></button>
+                    Show / Hide Nav
+                </div>
+
+                <div>
+                    Light / Dark
+                    {theme ?
+                        <button type="button" onClick={toggleTheme}>
+                        <i className="fa-regular fa-lightbulb ms-3 "></i>
+                    </button>
+                    :
+                        <button type="button" onClick={toggleTheme}>
+                        <i className="fa-solid fa-lightbulb ms-3 "></i>
+                    </button>
+                    }    
+                </div>    
+                    {/* <i className="fa-solid fa-toggle-on"></i>
+                    <i className="fa-solid fa-toggle-off"></i> */}
+  
             </header>
             <Toaster
                 position="top-right"
